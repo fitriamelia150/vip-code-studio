581fb7de196a99bfddf00396a3ce5d4e022564d3
fitria <user76@realtechltd.com>
2024-12-18 15:30:32 +0700

    useEffect

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react"
+import { Fragment, useState, useEffect } from "react"
 import Button from "../components/Elements/Button"
 import CardProduct from "../components/Fragments/CardProduct"
 
@@ -23,12 +23,24 @@ const products = [
 const email = localStorage.getItem("email")
 
 const ProductsPage = () => {
-    const [cart, setCart] = useState([
-        {
-            id:"",
-            qty: 0
+    const [cart, setCart] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem("cart")) || [])
+    },[])
+
+    useEffect(() => {
+        if(cart.length > 0) {
+            const sum = cart.reduce((acc, item) => {
+                const product = products.find((product) => product.id === item.id);
+                console.log(product.id)
+                return acc + product.price * item.qty
+            }, 0)
+            setTotalPrice(sum)
+            localStorage.setItem("cart", JSON.stringify(cart))
         }
-    ])
+    },[cart])
 
     const handleAddToCart = (id) => {
         if(cart.find(item => item.id === id)) {
@@ -57,7 +69,7 @@ const ProductsPage = () => {
 
             <div className="flex justify-center py-5 mx-2">
                 
-                <div className="w-3/4 flex">
+                <div className="w-2/2 flex">
                     {products.map((product) => (
                         <CardProduct key={product.id}>
                             <CardProduct.Header image={product.image}/>
@@ -69,7 +81,7 @@ const ProductsPage = () => {
                     ))}
                 </div>
 
-                <div className="w-1/4">
+                <div className="w-1/2">
                     <h1 className="text-3xl font-bold text-blue-600">Cart</h1>
 
                     <table className="text-left table-auto border-separate border-spacing-5">
@@ -82,9 +94,8 @@ const ProductsPage = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {console.log(cart)}
                              {cart.map((item)=>{
-                                if(item.id !== ""){
+                                // if(item.id !== ""){
                                     const product = products.find((product) => product.id === item.id);
                                     return (
                                         <tr key={item.id}>
@@ -94,8 +105,13 @@ const ProductsPage = () => {
                                             <td>Rp {(product.price * item.qty).toLocaleString("id-ID", {styles: 'currency', currency: 'IDR'})}</td>
                                         </tr>
                                     )
-                                }
+                                // }
                              })}
+
+                             <tr className="font-bold">
+                                <td colSpan={3}>Total Price</td>
+                                <td>Rp {totalPrice.toLocaleString("id-ID", {styles: 'currency', currency: 'IDR'})}</td>
+                             </tr>
                         </tbody>
                     </table>
                 </div>
