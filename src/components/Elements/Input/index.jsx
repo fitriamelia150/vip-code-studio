9b8798cdf7f6ea2451983803e0633f9891a1f79a
fitria <user76@realtechltd.com>
2024-12-19 15:40:01 +0700

    useRef

--- src/components/Elements/Input/index.jsx
+++ src/components/Elements/Input/index.jsx
@@ -1,14 +1,16 @@
 import Input from "./Input"
 import Label from "./Label"
+import { forwardRef } from "react"
 
-const InputForm = (props) => {
+// eslint-disable-next-line react/display-name
+const InputForm = forwardRef((props, ref) => {
     const { label, type, name, placeholder } = props
-    return (
-        <div className="mb-6">
-            <Label htmlFor={name}>{label}</Label>
-            <Input type={type} name={name} placeholder={placeholder}/>
-        </div>
-    )
-}
+        return (
+            <div className="mb-6">
+                <Label htmlFor={name}>{label}</Label>
+                <Input type={type} name={name} placeholder={placeholder} ref={ref}/>
+            </div>
+        )
+})
 
 export default InputForm
--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react"
 import Button from "../Elements/Button"
 import InputForm from "../Elements/Input"
 
@@ -10,14 +11,18 @@ const FormLogin = () => {
     window.location.href = "/products"
   }
 
+  const emailRef = useRef(null)
+  useEffect(()=>{
+    emailRef.current.focus()
+  },[])
 
-    return (
-        <form onSubmit={handleLogin}>
-          <InputForm label="Email" type="email" placeholder="example@gmail.com" name="email"/>
-          <InputForm label="Password" type="password" placeholder="****" name="password"/>  
-          <Button variant="bg-blue-600" type="submit">Login</Button>        
-        </form>
-    )
+  return (
+      <form onSubmit={handleLogin}>
+        <InputForm label="Email" type="email" placeholder="example@gmail.com" name="email" ref={emailRef}/>
+        <InputForm label="Password" type="password" placeholder="****" name="password"/>  
+        <Button variant="bg-blue-600" type="submit">Login</Button>        
+      </form>
+  )
 }
 
 export default FormLogin
